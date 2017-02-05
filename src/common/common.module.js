a347a27d41b04c6bf641ccadd329f74f6086b07f
Monika Mekala <Monika Mekala>
2017-02-04 21:38:33 -0500

    changes

--- src/common/common.module.js
+++ src/common/common.module.js
@@ -2,7 +2,7 @@
 "use strict";
 
 angular.module('common', [])
-.constant('ApiPath', 'http://momekala-restaurant.herokuapp.com')
+.constant('ApiPath', 'https://momekala-restaurant.herokuapp.com')
 .config(config);
 
 config.$inject = ['$httpProvider'];
